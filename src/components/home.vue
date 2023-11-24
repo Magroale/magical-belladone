20b5e918691118219b8ae2b5a58bfd7aa22a851b
Magroale <mario.alejo97@hotmail.com>
2023-11-24 12:28:50 -0500

    archivos importantes

---
+++ src/components/home.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="d-flex flex-row justify-content-evenly">
+        <div class="card" v-for="(imagen, indice) in imagenes" :key="indice">
+            <div class="bg-image hover-overlay" data-mdb-ripple-init data-mdb-ripple-color="light">
+                <img :src="imagen" class="img-fluid" alt="Imagen"/>
+                <a href="#!">
+                    <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+                </a>
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                    content.</p>
+            </div>
+        </div>
+    </div>
+</template>
+  
+<script>
+export default {
+    name: 'HelloWorld',
+    props: {
+        msg: String
+    },
+    data(){
+        return{
+            imagenes:[
+            "../assets/IMG_0625.JPG",
+            "../assets/IMG_0723.JPG",
+            "../assets/IMG_0760.JPG",
+            "../assets/IMG_0844.PNG"
+        ]
+        }
+    },
+    methods: {
+    },
+}
+</script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
+  
+  
---
+++ src/components/navBar.vue
@@ -0,0 +1,74 @@
+<template>
+  <nav data-mdb-navbar-init class="navbar navbar-expand-lg navbar-light bg-body-tertiary">
+    <!-- Container wrapper -->
+    <div class="container-fluid">
+      <!-- Toggle button -->
+
+
+
+      <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarCenteredExample"
+        aria-controls="navbarCenteredExample" aria-expanded="false" aria-label="Toggle navigation">
+        <i class="fas fa-bars"></i>
+      </button>
+
+      <!-- Collapsible wrapper -->
+      <div class="collapse navbar-collapse justify-content-center" id="navbarCenteredExample">
+        <a class="navbar-brand me-2" href="/">
+          <h1 class="text-center fs-2 text-wrap" style="max-width: 15rem;">Magical Belladone</h1>
+        </a>
+        <!-- Left links -->
+        <ul class="navbar-nav mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Sketch</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">About me</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Commission</a>
+          </li>
+          <!-- Navbar dropdown -->
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-mdb-toggle="dropdown"
+              aria-expanded="false">
+              Redes
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li>
+                <a class="dropdown-item" href="https://www.patreon.com/Magical_Belladone">Patreon</a>
+              </li>
+              <li>
+                <a class="dropdown-item" href="https://ko-fi.com/belladone">Ko-fi</a>
+              </li>
+              <li>
+                  <hr class="dropdown-divider" />
+                </li>
+              <li>
+                <a class="dropdown-item" href="https://twitter.com/MagicaBelladone">Twitter</a>
+              </li>
+              <li>
+                <a class="dropdown-item" href="https://www.facebook.com/MagicalBelladone">Facebook</a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+        <!-- Left links -->
+      </div>
+      <!-- Collapsible wrapper -->
+    </div>
+    <!-- Container wrapper -->
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
+
